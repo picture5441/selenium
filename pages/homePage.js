9394b3938033d4aaaff3059fecd58c820d4687d6
picture5441 <98991219+picture5441@users.noreply.github.com>
2022-11-20 21:12:10 +0300

    Commit By Intellectus

--- pages/homePage.js
+++ pages/homePage.js
@@ -3,17 +3,17 @@ const BasePage = require("./basePage");
 
 class HomePage extends BasePage {
 
-  async clickSearchButton() {
-    const element = await this.findByXpath(`//*[@class='search-icon']`);
+  function () {
+    const element = this.findByXpath(`//*[@class='search-icon']`);
     element.click();
     browser.wait(this.url("https://www.tiffany.com/"), 5000);
     return this;
   }
 
   
-  async inputSearchValue(text) {
+   function (text) {
 
-    await this.driver.findElement(By.xpath("//*[@id='searchInput']")).sendKeys(text, Key.ENTER);
+     this.driver.findElement(By.xpath(`//*[@id='searchInput']`)).sendKeys(text, Key.ENTER);
 
     return this;
   }
