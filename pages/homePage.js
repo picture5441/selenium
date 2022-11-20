b5fa3a6eb5cc882dd36875418e2f5538635a311d
picture5441 <98991219+picture5441@users.noreply.github.com>
2022-11-20 17:35:59 +0300

    Add files via upload

--- pages/homePage.js
+++ pages/homePage.js
@@ -2,11 +2,21 @@ const { By, Key } = require("selenium-webdriver");
 const BasePage = require("./basePage");
 
 class HomePage extends BasePage {
+
   async clickSearchButton() {
     const element = await this.findByXpath(`//*[@class='search-icon']`);
     element.click();
+
+    return this;
   }
 
+async clickSearchButton() {
+    const element = await this.findByXpath(`//*[@class='search-modal']`);
+    element.click();
+
+    return this;
+  }
+  
   async inputSearchValue(text) {
 
     await this.driver.findElement(By.xpath(`//input[@name='searchInput']`)).sendKeys(text, Key.ENTER);
